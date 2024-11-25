67b691c9c42169c0a5c68ea2ea2751c88e7a6aa0
$1 <$2>
2024-11-24 19:02:00 -0800

    remove obstacle befor starting

--- include/obstacle_adder.h
+++ include/obstacle_adder.h
@@ -6,6 +6,7 @@
 #include "geometry_msgs/msg/pose.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
 #include "std_msgs/msg/color_rgba.hpp"
+#include "std_srvs/srv/trigger.hpp"
 
 #include <random>
 
@@ -16,26 +17,14 @@ public:
     void add_random_cubes();
 
 private:
-    rclcpp::Client<curobo_msgs::srv::AddObject>::SharedPtr client_;
+    rclcpp::Client<curobo_msgs::srv::AddObject>::SharedPtr client_add_obj;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr client_remove_obj;
     std::mt19937 rng_; // Générateur de nombres aléatoires
     std::uniform_int_distribution<int> num_cubes_dist_; // Distribution pour le nombre de cubes
     std::uniform_real_distribution<double> position_dist_; // Distribution pour les positions
     std::uniform_real_distribution<double> size_dist_; // Distribution pour les dimensions
 
-    /// @brief Verify the cube to add is not in the forbidden zone
-    /// @param x the x position of the cube
-    /// @param y the y position of the cube
-    /// @param z the z position of the cube
-    /// @param size_x the size of the cube in x
-    /// @param size_y the size of the cube in y
-    /// @param size_z 
-    /// @param x_min the x position of the forbidden zone
-    /// @param x_max the x position of the forbidden zone
-    /// @param y_min the y position of the forbidden zone
-    /// @param y_max the y position of the forbidden zone
-    /// @param z_min the z position of the forbidden zone
-    /// @param z_max the z position of the forbidden zone
-    /// @return true if the cube is in the forbidden zone, false otherwise
+
     bool is_in_forbidden_zone(double x, double y, double z, 
                                             double size_x, double size_y, double size_z, 
                                             double x_min, double x_max, double y_min, double y_max, double z_min, double z_max);
--- src/obstacle_adder.cpp
+++ src/obstacle_adder.cpp
@@ -5,10 +5,21 @@
 ObstacleAdder::ObstacleAdder() : Node("obstacle_adder")
 {
     // Création du client pour le service "add_obstacle"
-    client_ = this->create_client<curobo_msgs::srv::AddObject>("/curobo_gen_traj/add_object");
+    client_add_obj = this->create_client<curobo_msgs::srv::AddObject>("/curobo_gen_traj/add_object");
 
+    while (!client_add_obj->wait_for_service(std::chrono::seconds(1)))
+    {
+        if (!rclcpp::ok())
+        {
+            RCLCPP_ERROR(this->get_logger(), "Interrompu pendant l'attente du service. Sortie.");
+            return;
+        }
+        RCLCPP_INFO(this->get_logger(), "Service non disponible, nouvelle tentative...");
+    }
+
+    client_remove_obj = this->create_client<std_srvs::srv::Trigger>("/curobo_gen_traj/remove_all_objects"); // trigger
     // Attendre que le service soit disponible
-    while (!client_->wait_for_service(std::chrono::seconds(1)))
+    while (!client_remove_obj->wait_for_service(std::chrono::seconds(1)))
     {
         if (!rclcpp::ok())
         {
@@ -23,15 +34,28 @@ ObstacleAdder::ObstacleAdder() : Node("obstacle_adder")
     rng_ = std::mt19937(rd());
 
     // Définir les distributions
-    this->num_cubes_dist_ = std::uniform_int_distribution<int>(1, 20); // Nombre de cubes entre 1 et 10
-    this->position_dist_ = std::uniform_real_distribution<double>(-1.0, 1.0); // Positions entre -2 et 2
-    this->size_dist_ = std::uniform_real_distribution<double>(0.1, 1.0); // Tailles entre 0.1 et 1.0 mètres
+    this->num_cubes_dist_ = std::uniform_int_distribution<int>(1, 5);        // Nombre de cubes entre 1 et 10
+    this->position_dist_ = std::uniform_real_distribution<double>(-0.90, 0.9); // Positions entre -2 et 2
+    this->size_dist_ = std::uniform_real_distribution<double>(0.1, 0.5);      // Tailles entre 0.1 et 1.0 mètres
 }
 
-
-bool ObstacleAdder::is_in_forbidden_zone(double x, double y, double z, 
-                                            double size_x, double size_y, double size_z, 
-                                            double x_min, double x_max, double y_min, double y_max, double z_min, double z_max)
+/// @brief Verify the cube to add is not in the forbidden zone
+/// @param x the x position of the cube
+/// @param y the y position of the cube
+/// @param z the z position of the cube
+/// @param size_x the size of the cube in x
+/// @param size_y the size of the cube in y
+/// @param size_z
+/// @param x_min the x position of the forbidden zone
+/// @param x_max the x position of the forbidden zone
+/// @param y_min the y position of the forbidden zone
+/// @param y_max the y position of the forbidden zone
+/// @param z_min the z position of the forbidden zone
+/// @param z_max the z position of the forbidden zone
+/// @return true if the cube is in the forbidden zone, false otherwise
+bool ObstacleAdder::is_in_forbidden_zone(double x, double y, double z,
+                                         double size_x, double size_y, double size_z,
+                                         double x_min, double x_max, double y_min, double y_max, double z_min, double z_max)
 
 {
     // calculer les coordonnées des coins du cube
@@ -41,23 +65,47 @@ bool ObstacleAdder::is_in_forbidden_zone(double x, double y, double z,
     double y_cube_max = y + size_y / 2;
     double z_cube_min = z - size_z / 2;
     double z_cube_max = z + size_z / 2;
-
-   // si xa ou xb sont compris entre x1 et x2 ET ya ou yb sont compris entre y1 et y2 ET za ou zb sont compris entre z1 et z2    
-    if ((x_min >= x_cube_min && x_min <= x_cube_max) || (x_max >= x_cube_min && x_max <= x_cube_max))
+    
+    // x >= start && x <= end
+    x_cube_min <= x_min && x_min <= x_cube_max
+    // si xa ou xb sont compris entre x1 et x2 ET ya ou yb sont compris entre y1 et y2 ET za ou zb sont compris entre z1 et z2
+    if ((x_cube_min <= x_min && x_min <= x_cube_max) || (x_cube_min <= x_max && x_max <= x_cube_max))
     {
-        if ((y_min >= y_cube_min && y_min <= y_cube_max) || (y_max >= y_cube_min && y_max <= y_cube_max))
+        if ((y_cube_min <= y_min && y_min <= y_cube_max) || (y_cube_min <= y_max && y_max <= y_cube_max))
         {
-            if ((z_min >= z_cube_min && z_min <= z_cube_max) || (z_max >= z_cube_min && z_max <= z_cube_max))
+            if ((z_cube_min <= z_min && z_min <= z_cube_max) || (z_cube_min <= z_max && z_max <= z_cube_max))
             {
                 return true;
             }
-        }   
+            
+        }
+       
     }
-    return false; 
+    return false;
 }
 
 void ObstacleAdder::add_random_cubes()
 {
+    // Supprimer tous les obstacles existants
+    auto request_remove = std::make_shared<std_srvs::srv::Trigger::Request>();
+    auto future_remove = client_remove_obj->async_send_request(request_remove);
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), future_remove) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+        auto response = future_remove.get();
+        if (response->success)
+        {
+            RCLCPP_INFO(this->get_logger(), "Tous les obstacles ont été supprimés.");
+        }
+        else
+        {
+            RCLCPP_ERROR(this->get_logger(), "Échec de la suppression des obstacles : %s", response->message.c_str());
+        }
+    }
+    else
+    {
+        RCLCPP_ERROR(this->get_logger(), "Échec de l'appel au service remove_all_object");
+    }
     // Déterminer un nombre aléatoire de cubes à ajouter
     int num_cubes = this->num_cubes_dist_(rng_);
     RCLCPP_INFO(this->get_logger(), "Ajout de %d cube(s) aléatoire(s).", num_cubes);
@@ -76,8 +124,8 @@ void ObstacleAdder::add_random_cubes()
             cube_size = this->size_dist_(rng_);
             // print size
             RCLCPP_INFO(this->get_logger(), "Cube size %f", cube_size);
-        } while (this->is_in_forbidden_zone(x, y, z, cube_size, cube_size, cube_size, -0.2, 0.2, -0.2, 0.2, -1.45, 1.45));
-      
+        } while (!this->is_in_forbidden_zone(x, y, z, cube_size, cube_size, cube_size, -0.3, 0.3, -0.3, 0.3, -1.45, 1.45));
+
         // Création de la requête
         auto request = std::make_shared<curobo_msgs::srv::AddObject::Request>();
 
@@ -109,7 +157,7 @@ void ObstacleAdder::add_random_cubes()
         request->color.a = 1.0f; // Opacité totale
 
         // Envoyer la requête de manière asynchrone
-        auto future = client_->async_send_request(request);
+        auto future = client_add_obj->async_send_request(request);
 
         // Attendre le résultat
         if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), future) ==
@@ -119,7 +167,6 @@ void ObstacleAdder::add_random_cubes()
             if (response->success)
             {
                 RCLCPP_INFO(this->get_logger(), "Obstacle '%s' ajouté avec succès. Size %f, x = %f, y = %f, z = %f", request->name.c_str(), cube_size, x, y, z);
-                
             }
             else
             {
@@ -132,7 +179,6 @@ void ObstacleAdder::add_random_cubes()
         }
     }
 }
-
 
 int main(int argc, char **argv)
 {
