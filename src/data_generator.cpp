0c4087508ee666ab48f8f6b02ba514c2c0a1596d
will-44 <guillaume.dupoiron@gmail.com>
2025-01-08 10:11:11 -0800

    clean code

--- src/data_generator.cpp
+++ src/data_generator.cpp
@@ -1,103 +1,3 @@
-// //
-// // Created by will on 04/04/24.
-// //
-
-// #include "../include/data_generator.h"
-
-// DataGenerator::DataGenerator() : Node("data_generator"), robot(this->shared_from_this())
-// {
-//     // robot = Robot(this->shared_from_this());
-//     this->service_ = this->create_service<curobo_msgs::srv::GenerateRM>(
-//         "generate_rm", std::bind(&DataGenerator::callback_generate_rm, this, std::placeholders::_1, std::placeholders::_2));
-// }
-
-// DataGenerator::~DataGenerator() {}
-
-// void DataGenerator::callback_generate_rm(
-//     const std::shared_ptr<curobo_msgs::srv::GenerateRM::Request> request,
-//     std::shared_ptr<curobo_msgs::srv::GenerateRM::Response> response)
-// {
-//     float resolution = request->resolution;
-//     std::vector<geometry_msgs::msg::Pose> data;
-
-//     std::stringstream resolution_string;
-//     resolution_string << resolution;              // appending the float value to the streamclass
-//     std::string result = resolution_string.str(); // converting the float value to string
-
-//     utils::load_poses_from_file(ament_index_cpp::get_package_share_directory("data_generation") + "/data" + "/master_ik_data" + result + ".npz", data);
-
-//     // split data into batches
-//     int batch_size = request->batch_size;
-//     std::vector<std::vector<geometry_msgs::msg::Pose>> batches;
-//     utils::split_data(data, batch_size, batches);
-
-//     // create a master ik data object
-//     MasterIkData ik_data;
-
-//     // iterate all batches
-//     for (const auto &batch : batches)
-//     {
-//         // send the batch to robot ik
-//         std::vector<sensor_msgs::msg::JointState> joint_states;
-//         std::vector<std_msgs::msg::Bool> joint_states_valid;
-//         robot.get_iks(batch, joint_states, joint_states_valid);
-//         int nb_valide = 0;
-//         for (size_t i = 0; i < joint_states.size(); i++)
-//         {
-//             // from ik_data get the sphere with key x, y, z
-//             if (joint_states_valid[i].data)
-//             {
-//                 // create a pose in the sphere
-//                 PoseOnSphere p;
-//                 p.x = utils::round_to_decimals(batch[i].position.x, 3);
-//                 p.y = utils::round_to_decimals(batch[i].position.y, 3);
-//                 p.z = utils::round_to_decimals(batch[i].position.z, 3);
-//                 p.theta_x = utils::round_to_decimals(batch[i].orientation.x, 6);
-//                 p.theta_y = utils::round_to_decimals(batch[i].orientation.y, 6);
-//                 p.theta_z = utils::round_to_decimals(batch[i].orientation.z, 6);
-//                 p.theta_w = utils::round_to_decimals(batch[i].orientation.w, 6);
-//                 // if joint[i] is not empty add the joints to the pose
-//                 nb_valide++;
-//                 // create a joint
-//                 joint join;
-//                 join.j1 = joint_states[i].position[0];
-//                 join.j2 = joint_states[i].position[1];
-//                 join.j3 = joint_states[i].position[2];
-//                 join.j4 = joint_states[i].position[3];
-//                 join.j5 = joint_states[i].position[4];
-//                 join.j6 = joint_states[i].position[5];
-//                 // add the joint to the pose
-//                 std::vector<joint> joints = {join};
-//                 p.add(joints);
-//                 // add the pose to the sphere
-//                 ik_data.update_sphere(batch[i].position.x, batch[i].position.y, batch[i].position.z, p);
-//             }
-//         }
-//     }
-//     // rnd name for file
-
-//     auto t = std::time(nullptr);
-//     auto tm = *std::localtime(&t);
-//     // std::string date = std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
-//     std::ostringstream oss;
-//     oss << std::put_time(&tm, "%d_%m_%Y_%H_%M_%S");
-//     auto date_str = oss.str();
-//     ik_data.write_data(ament_index_cpp::get_package_share_directory("data_generation") + "/data/" + date_str + "_" + result + ".json");
-
-//     response->success = true;
-//     response->message = "Reachability map generated successfully.";
-
-// }
-
-// int main(int argc, char *argv[])
-// {
-//     rclcpp::init(argc, argv);
-
-//     rclcpp::spin(std::make_shared<DataGenerator>());
-//     rclcpp::shutdown();
-//     return 0;
-// }
-
 #include <chrono>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
@@ -119,7 +19,7 @@
 
 #include <boost/multi_array.hpp>
 #include <highfive/highfive.hpp>
-// #include <highfive/boost.hpp>
+
 
 #include <iostream>
 using namespace std;
@@ -241,12 +141,19 @@ namespace cb_data_generator
                     data_index += 1;
                 }
             }
-            this->saveToHDF5(data_result, resolution);
+            std::vector<std::array<double, 3>> voxel_map = {};
+            this->get_voxel_map(voxel_map);
+            this->saveToHDF5(data_result, voxel_map, resolution);
 
             return true;
         }
 
-        void saveToHDF5(const std::vector<std::array<double, 4>> &data,  float voxel_size)
+        void get_voxel_map(const std::vector<std::array<double, 3>> &voxel_map){
+            // ask voxel map to curobo
+            // this voxel map is n*4 with x, y, z, state (0 = free, 1 = occupied)
+        }
+
+        void saveToHDF5(const std::vector<std::array<double, 4>> &data, const std::vector<std::array<double, 3>> &voxel_map,  float voxel_size)
         {
             using namespace HighFive;
 
@@ -274,8 +181,8 @@ namespace cb_data_generator
         std::string data_file_path;
         int dataset_id;
 
-    }; // class DemoNode
-} // namespace cb_group_demo
+    }; 
+} 
 
 int main(int argc, char *argv[])
 {
