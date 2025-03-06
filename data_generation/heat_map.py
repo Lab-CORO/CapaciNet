ba926508e844992c0518be1c51a7934a93152b3b
will-44 <guillaume.dupoiron@gmail.com>
2025-03-06 06:19:31 -0800

    show heat map

--- data_generation/heat_map.py
+++ data_generation/heat_map.py
@@ -4,28 +4,30 @@ import rclpy
 from rclpy.node import Node
 import h5py
 import numpy as np
-from visualization_msgs.msg import Marker
+from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point
 import std_msgs.msg
 
 import matplotlib.pyplot as plt
 
 # Path to the HDF5 file
-H5_FILE_PATH = "/home/ros2_ws/install/data_generation/share/data_generation/data/test_0_5_vae.h5"
+H5_FILE_PATH = "/home/ros2_ws/install/data_generation/share/data_generation/data/05_03_2025_17_57_43.h5.h5"
 
 class VoxelMapPublisher(Node):
     def __init__(self):
         super().__init__('voxel_map_publisher')
 
         # Create a publisher for the voxel map using CUBE_LIST
-        self.publisher = self.create_publisher(Marker, 'voxel_map', 10)
+        self.publisher = self.create_publisher(MarkerArray, 'voxel_map', 10)
 
         # Timer to publish data
         self.timer = self.create_timer(1.0, self.publish_voxel_map)
 
         # Load voxel data from the HDF5 file
-        self.voxel_map = self.load_voxel_map()
         self.voxel_data = []
+        self.group_nb = 0 
+        self.voxel_map = self.load_voxel_map()
+        
         id = 0
 
     def load_voxel_map(self):
@@ -34,7 +36,7 @@ class VoxelMapPublisher(Node):
 
         with h5py.File(H5_FILE_PATH, 'r') as h5_file:
             groups = h5_file['group']
-
+            self.group_nb = len(groups)
             for group_name in groups:
                 self.voxel_data = np.array(groups[group_name]['voxel_map']['data'])
 
@@ -47,37 +49,44 @@ class VoxelMapPublisher(Node):
 
     def publish_voxel_map(self):
         """Creates and publishes a single CUBE_LIST marker for RViz."""
-        marker = Marker()
-        marker.header.frame_id = "map"
-        marker.header.stamp = self.get_clock().now().to_msg()
-        marker.ns = "voxel_map"
-        marker.id = 0
-        marker.type = Marker.CUBE_LIST  # Using CUBE_LIST for efficiency
-        marker.action = Marker.ADD
-
-        marker.scale.x = 0.2
-        marker.scale.y = 0.2
-        marker.scale.z = 0.2
-
-        marker.pose.orientation.w = 1.0  # Identity quaternion
+        marker_array = MarkerArray()
+        id = 0
         intensities = []
         for (x, y, z), intensity in self.voxel_map.items():
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.ns = "voxel_map"
+            marker.id = id
+            id +=1
+            marker.type = Marker.CUBE  # Using CUBE_LIST for efficiency
+            marker.action = Marker.ADD
+
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+
+            marker.pose.orientation.w = 1.0  # Identity quaternion
+            marker.pose.position.x = x  # Identity quaternion
+            marker.pose.position.y = y  # Identity quaternion
+            marker.pose.position.z = z  # Identity quaternion
+            
             intensities.append(intensity)
-            point = Point()
-            point.x = x
-            point.y = y
-            point.z = z
-            marker.points.append(point)
 
+            # print(intensity)
             # Color based on intensity (normalized)
-            color_intensity = min(intensity / 955, 1.0)  # Normalize (cap at 1.0)
-            marker.color.r = (0.0, 1.0)[intensity == 1.0]
+            color_intensity = min(intensity / 150, 1.0)  # Normalize (cap at 1.0)
+            print(color_intensity)
+            marker.color.r = color_intensity #(0.0, 1.0)[intensity == 1.0]
             marker.color.g = 0.0
-            marker.color.b = 0.0 #- color_intensity
-            marker.color.a = 0.5  # Fully opaque
+            marker.color.b = 0.0 #1.0 - color_intensity
+            marker.color.a = (0.0, 1.0)[color_intensity == 1.0]  # Fully opaque
 
-        self.publisher.publish(marker)
-        self.get_logger().info(f"Published {len(marker.points)} voxels.")
+            marker_array.markers.append(marker)
+        print(self.group_nb)
+
+        self.publisher.publish(marker_array)
+        # self.get_logger().info(f"Published {len(marker_array.markers)} voxels.")
         # print(intensities)
         # input("Press Enter to continue...")
         # with matplotlib plot a graph with the number of occupy voxel (color intensity) and free voxel
