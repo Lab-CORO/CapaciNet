948759ea30eab39b2d2f93b38e39dad5a2347ff8
will-44 <guillaume.dupoiron@protonmail.com>
2025-10-14 13:26:32 -0700

    IRM compile and run (need more test)

--- data_generation/src/create_inverse_reachability_map.cpp
+++ data_generation/src/create_inverse_reachability_map.cpp
@@ -8,10 +8,9 @@
 #include <octomap/MapCollection.h>
 #include <octomap/math/Utils.h>
 
-#include <map_creator/sphere_discretization.h>
-#include <map_creator/kinematics.h>
-#include <map_creator/hdf5_dataset.h>
-#include "map_creator/msg/work_space.hpp"
+#include "../include/sphere_discretization.h"
+#include "../include/utils.h"
+// #include "map_creator/msg/work_space.hpp"
 
 #include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Quaternion.h>
@@ -24,6 +23,13 @@
 #include <time.h>
 #include <filesystem>
 
+// --- MultiMapPtr : mapping entre points de base et poses accessibles
+typedef std::multimap<const geometry_msgs::msg::Point*, const geometry_msgs::msg::Pose*> MultiMapPosePtr;
+
+// --- MapVecDoublePtr : associe une position (clé) à un score
+typedef std::map<const geometry_msgs::msg::Point*, double> MapPointDoublePtr;
+
+
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
@@ -32,89 +38,30 @@ int main(int argc, char **argv)
   time_t startit, finish;
   time(&startit);
   
-  kinematics::Kinematics k(node);
   std::string file;
   std::string pkg_share_dir;
-  
-  try {
-    pkg_share_dir = ament_index_cpp::get_package_share_directory("map_creator");
-  } catch (const std::exception& e) {
-    RCLCPP_ERROR(node->get_logger(), "Failed to get package directory: %s", e.what());
-    return 1;
-  }
-  
-  std::string path = pkg_share_dir + "/maps/";
-  std::string filename;
-  const char *input_FILE;
 
-  if (argc < 2)
-  {
-    RCLCPP_ERROR(node->get_logger(), 
-      "Please provide the name of the reachability map. If you have not created it yet, "
-      "Please create the map by running the create reachability map node in map_creator package");
-    return 0;
-  }
-  else if (argc == 2)
-  {
-    RCLCPP_INFO(node->get_logger(), "Creating map with default name.");
-    input_FILE = argv[1];
-    
-    if (!std::filesystem::exists(input_FILE))
-    {
-      RCLCPP_ERROR(node->get_logger(), "Input file does not exist");
-      return false;
-    }
-    else
-    {
-      float res;
-      hdf5_dataset::Hdf5Dataset h5_res(argv[1]);
-      h5_res.open();
-      h5_res.h5ToResolution(res);
-      h5_res.close();
-      
-      file = str(boost::format("%s_r%d_Inv_reachability.h5") % k.getRobotName() % res);
-      filename = path + file;
-    }
-  }
-  else if (argc == 3)
-  {
-    input_FILE = argv[1];
-    std::string str(argv[2]);
-    
-    if (!std::filesystem::exists(input_FILE))
-    {
-      RCLCPP_ERROR(node->get_logger(), "Input file does not exist");
-      return false;
-    }
-    else
-    {
-      if (std::strchr(str.c_str(), '/'))
-      {
-        filename = argv[2];
-      }
-      else
-        filename = path + str;
-    }
-  }
 
   rclcpp::Rate loop_rate(10);
 
   int count = 0;
   while (rclcpp::ok())
   {
-    MultiMapPtr pose_col_filter;
-    MapVecDoublePtr sphere_col;
-    float res;
+    // std::vector<geometry_msgs::msg::Pose> pose_col_filter;
+    std::vector<geometry_msgs::msg::Pose> pose_col_filter;
+    MapPointDoublePtr sphere_col;
+    float res = 0.1;
 
-    hdf5_dataset::Hdf5Dataset h5file(input_FILE);
-    h5file.open();
-    h5file.loadMapsFromDataset(pose_col_filter, sphere_col, res);
+    // hdf5_dataset::Hdf5Dataset h5file(input_FILE);
+    // h5file.open();
+    // h5file.loadMapsFromDataset(pose_col_filter, sphere_col, res);
+    utils::load_poses_from_file("/home/ros2_ws/src/CapaciNet/data_generation/data/master_ik_data0.5.npz", pose_col_filter);
 
     // Create Inverse Reachability map
     unsigned char max_depth = 16;
     unsigned char minDepth = 0;
     float size_of_box = 1.5;
-    float resolution = res;
+    float resolution = 0.1;
     sphere_discretization::SphereDiscretization sd;
 
     octomap::point3d origin = octomap::point3d(0, 0, 0);
@@ -122,7 +69,11 @@ int main(int argc, char **argv)
     std::vector<octomap::point3d> new_data;
 
     std::vector<geometry_msgs::msg::Pose> pose;
-    sd.make_sphere_poses(origin, resolution, pose);
+    geometry_msgs::msg::Point orgine_pt;
+    orgine_pt.x = 0;
+    orgine_pt.y = 0;
+    orgine_pt.z = 0;
+    sd.make_sphere_poses(orgine_pt, resolution, pose);
 
     for (octomap::OcTree::leaf_iterator it = tree->begin_leafs(max_depth), end = tree->end_leafs(); 
          it != end; ++it)
@@ -138,10 +89,10 @@ int main(int argc, char **argv)
 
     std::multimap<std::vector<float>, std::vector<float>> trns_col;
     
-    for (MultiMapPtr::iterator it = pose_col_filter.begin(); it != pose_col_filter.end(); ++it)
+    for (std::vector<geometry_msgs::msg::Pose>::iterator it = pose_col_filter.begin(); it != pose_col_filter.end(); ++it)
     {
-      tf2::Vector3 vec((*it->second)[0], (*it->second)[1], (*it->second)[2]);
-      tf2::Quaternion quat((*it->second)[3], (*it->second)[4], (*it->second)[5], (*it->second)[6]);
+      tf2::Vector3 vec(it->position.x, it->position.y, it->position.z);
+      tf2::Quaternion quat(it->orientation.x, it->orientation.y, it->orientation.z, it->orientation.w);
       tf2::Transform trns;
       trns.setOrigin(vec);
       trns.setRotation(quat);
@@ -174,12 +125,12 @@ int main(int argc, char **argv)
       point.z = inv_trans_vec[2];
       cloud->push_back(point);
     }
-
-    MultiMapPtr base_trns_col;
+    MultiMapPosePtr base_trns_col;
     pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree(resolution);
     octree.setInputCloud(cloud);
     octree.addPointsFromInputCloud();
-    
+    // apres ici
+// RCLCPP_INFO(node->get_logger(), "Number of voxels:");
     for (size_t i = 0; i < new_data.size(); i++)
     {
       pcl::PointXYZ search_point;
@@ -190,14 +141,12 @@ int main(int argc, char **argv)
       // Neighbors within voxel search
       std::vector<int> point_idx_vec;
       octree.voxelSearch(search_point, point_idx_vec);
-
       if (point_idx_vec.size() > 0)
       {
-        std::vector<double>* base_sphere = new std::vector<double>();
-        base_sphere->push_back(search_point.x);
-        base_sphere->push_back(search_point.y);
-        base_sphere->push_back(search_point.z);
-
+        geometry_msgs::msg::Point* base_sphere = new geometry_msgs::msg::Point(); 
+        base_sphere->x = search_point.x;
+        base_sphere->y = search_point.y;
+        base_sphere->z = search_point.z;
         for (size_t j = 0; j < point_idx_vec.size(); ++j)
         {
           std::vector<float> base_pos;
@@ -207,25 +156,25 @@ int main(int argc, char **argv)
 
           for (auto it1 = trns_col.lower_bound(base_pos); it1 != trns_col.upper_bound(base_pos); ++it1)
           {
-            std::vector<double>* base_pose = new std::vector<double>();
-            base_pose->push_back(base_pos[0]);
-            base_pose->push_back(base_pos[1]);
-            base_pose->push_back(base_pos[2]);
-            base_pose->push_back(it1->second[0]);
-            base_pose->push_back(it1->second[1]);
-            base_pose->push_back(it1->second[2]);
-            base_pose->push_back(it1->second[3]);
+            geometry_msgs::msg::Pose* base_pose = new geometry_msgs::msg::Pose();
+            base_pose->position.x = (base_pos[0]);
+            base_pose->position.y = (base_pos[1]);
+            base_pose->position.z = (base_pos[2]);
+            base_pose->orientation.x =(it1->second[0]);
+            base_pose->orientation.y = (it1->second[1]);
+            base_pose->orientation.z = (it1->second[2]);
+            base_pose->orientation.w = (it1->second[3]);
 
             base_trns_col.insert(std::make_pair(base_sphere, base_pose));
           }
         }
       }
     }
-
-    MapVecDoublePtr sphere_color;
-    for (MultiMapPtr::iterator it = base_trns_col.begin(); it != base_trns_col.end(); ++it)
+// avanrt ICI
+    MapPointDoublePtr sphere_color;
+    for (MultiMapPosePtr::iterator it = base_trns_col.begin(); it != base_trns_col.end(); ++it)
     {
-      const std::vector<double>* sphere_coord = it->first;
+      const geometry_msgs::msg::Point* sphere_coord = it->first;
       float d = (float(base_trns_col.count(sphere_coord)) / pose.size()) * 100;
       sphere_color.insert(std::make_pair(it->first, double(d)));
     }
@@ -236,8 +185,8 @@ int main(int argc, char **argv)
     RCLCPP_INFO(node->get_logger(), 
       "All the poses have been processed. Now saving data to an inverse Reachability Map.");
 
-    hdf5_dataset::Hdf5Dataset irm_h5(filename);
-    irm_h5.saveReachMapsToDataset(base_trns_col, sphere_color, res);
+    // hdf5_dataset::Hdf5Dataset irm_h5(filename);
+    // irm_h5.saveReachMapsToDataset(base_trns_col, sphere_color, res);
 
     time(&finish);
     double dif = difftime(finish, startit);
