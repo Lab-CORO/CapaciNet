f0f36075f83f27dd4fc79ce3cb0845c26953eaee
will-44 <guillaume.dupoiron@protonmail.com>
2026-06-01 17:49:35 -0400

    Fix start.bash: update image name and volume path for Jetson setup
    - Image: capacitynet → capacitynet_aarch:latest
    - Volume: $PWD/:/workspace/capacitynet/ → ./ros2_ws/src/capacitynet/:/home/ros2_ws/src/capacitynet/

--- unet_3d/start.bash
+++ unet_3d/start.bash
@@ -4,5 +4,5 @@ xhost +local:docker
 
 docker run --name capacitynet2 -it --gpus all --network=host \
   --env DISPLAY=$DISPLAY  \
-  --volume  $PWD/:/workspace/capacitynet/ \
-  capacitynet
+  --volume  ./ros2_ws/src/capacitynet/:/home/ros2_ws/src/capacitynet/ \
+  capacitynet_aarch:latest
